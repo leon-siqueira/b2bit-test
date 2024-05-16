a1871383495a7beadc49ee586aab50f9f2ce80f1
Leon Siqueira <leon.siqueir4@gmail.com>
2024-05-15 21:30:14 -0300

    feat: added timeout message

--- src/helpers/commonErrorMessages.tsx
+++ src/helpers/commonErrorMessages.tsx
@@ -1,6 +1,7 @@
 export function commonErrorMessages(code: number) : string {
   const errors = {
     401: 'You\'re not allowed to access this page. Please log in.',
+    408: 'Request timeout',
     500: 'An error occurred on the server. Please try again later',
   } as Record<number, string>;
 
--- src/helpers/loginErrorMessages.tsx
+++ src/helpers/loginErrorMessages.tsx
@@ -1,6 +1,7 @@
 export function loginErrorMessages(code: number) : string {
   const errors = {
     401: 'Your credentials are invalid',
+    408: 'Request timeout',
     500: 'An error occurred on the server. Please try again later',
   } as Record<number, string>;
 
