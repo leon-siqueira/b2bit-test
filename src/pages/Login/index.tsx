dd5ad60147736cfb7e9d22c5d58309892a7c53cc
Leon Siqueira <leon.siqueir4@gmail.com>
2024-05-18 22:35:40 -0300

    feat: redirect already logged users

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,6 +1,16 @@
+import { useNavigate } from "react-router-dom";
 import LoginForm from "../../components/LoginForm";
+import useNotify from "../../hooks/useNotify";
 
 export default function User() {
+  const notify = useNotify()
+  const navigate = useNavigate()
+
+  if(localStorage.getItem('accessToken')) {
+    notify({ message: 'You are already logged in.', kind: 'info' })
+    navigate('/')
+  }
+
   return (
     <div className="flex items-center justify-center min-h-dvh bg-gray-100">
       <LoginForm />
