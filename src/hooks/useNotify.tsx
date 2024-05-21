ae32ad293e03dea9a9fee71dc030e543cfe2075e
Leon Siqueira <leon.siqueir4@gmail.com>
2024-05-20 23:42:20 -0300

    refactor: components with imported types

--- src/hooks/useNotify.tsx
+++ src/hooks/useNotify.tsx
@@ -1,12 +1,13 @@
 import { useContext } from "react";
-import { NotificationContext, NotificationContextType, NotificationType } from "../main";
+import { NotificationContext } from "../main";
+import { Notification } from "../types/notification";
+import { NotificationContextType } from "../types/notificationContextType";
 
-export default function useNotify() : (notification: NotificationType) => void {
+export default function useNotify() : (notification: Notification) => void {
   useContext(NotificationContext) as NotificationContextType
 
   const { setNotification } = useContext(NotificationContext) as NotificationContextType
-
-  const notify = (notification: NotificationType) => {
+  const notify = (notification: Notification) => {
     setNotification(notification)
   }
 
